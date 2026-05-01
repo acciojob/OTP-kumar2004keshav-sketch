09eef12790293cfa5cb2a6d8a519563ed2025626
Keshav Kumar <kumar2004keshav@gmail.com>
2026-05-01 12:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+ 
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+
+            codes[idx].value = '';
+
+            setTimeout(() => codes[idx + 1]?.focus(), 10);
+        } else if (e.key === 'Backspace') {
+            // Move focus to previous field on backspace
+            setTimeout(() => codes[idx - 1]?.focus(), 10);
+        }
+    });
+
+
+    code.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (value.length > 1) {
+            e.target.value = value.slice(-1);
+        }
+    });
+});
